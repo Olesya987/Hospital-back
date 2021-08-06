54a0c5edd8b637967b13823bea15cc0e911ef748
Olesya987 <oderevianko.exceedteam@gmail.com>
2021-08-06 13:12:11 +0300

    add post appointment

--- src/modules/controllers/appointment.controllers.js
+++ src/modules/controllers/appointment.controllers.js
@@ -1,23 +1,23 @@
 const Appointment = require("../../DB/models/hospital/appointments");
 
-module.exports.getAppointments = (req, res) => {
+module.exports.getAppointment = (req, res) => {
   Appointment.find().then((result) => res.send({ appointments: result }));
 };
 
-// module.exports.postSpend = async (req, res) => {
-//   const { body } = req;
-//   const value = new Outlay(body);
-//   if (value.where && value.date && value.howMuch) {
-//     value
-//       .save()
-//       .then((result) => {
-//         Outlay.find().then((result) => res.send({ costs: result }));
-//       })
-//       .catch((err) => res.send(err));
-//   } else {
-//     res.send("User creation error, not all fields are filled");
-//   }
-// };
+module.exports.postAppointment = async (req, res) => {
+  const { body } = req;
+  const value = new Appointment(body);
+  if (value.name.length!=0 && value.date.length!=0 && value.docName.length!=0) {
+    value
+      .save()
+      .then((result) => {
+        Appointment.find().then((result) => res.send({ appointments: result }));
+      })
+      .catch((err) => res.send(err));
+  } else {
+    res.send("Appointment creation error, not all fields are filled");
+  }
+};
 
 // module.exports.patchSpend = async (req, res) => {
 //   const { body } = req;
